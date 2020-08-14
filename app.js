dd44e591a9240353323426c2d722a30c690619a5
Daniel <d19990511@gmail.com>
2020-08-14 12:10:45 +0800

    First

--- app.js
+++ app.js
@@ -17,18 +17,22 @@ let app = express();
 app.use(express.static('./public'));
 app.set('view engine', 'ejs');  
 app.get('/', async (req, res) => {  
-    //let data = await db.collection('classA').get();
-    //let userArr = []
-    // data.forEach((doc) => {
-    //     console.log(doc.data().name)
-    //     userArr.push(doc.data().name);
-    // })
+    let data = await db.collection('classA').get();
+    let userArr = []
+    data.forEach((doc) => {
+        console.log(doc.data().name)
+        userArr.push(doc.data().name);
+    })
     res.render('default', {  
         title: '首頁',  
-        users: ['Fisheep', 'Fiona', 'Alice', 'Bob']
-        //users: userArr
+        //users: ['Fisheep', 'Fiona', 'Alice', 'Bob']
+        users: userArr
     });  
 });
+
+app.get("/Daniel",(req,res) => {
+    res.send("<h1>Daniel</h1>");
+})
 
 app.get("/firebase-test", async (req, res) => {
     let html = '';
--- render_test.js
+++ render_test.js
@@ -1,9 +1,29 @@
 let express = require('express')
 let app = express();
+let firebase = require('firebase')
+var firebaseConfig = {
+    apiKey: "AIzaSyDa-wza5N7azOMS4_CY-vG1464BheWM8aM",
+    authDomain: "myproject-7c265.firebaseapp.com",
+    databaseURL: "https://myproject-7c265.firebaseio.com",
+    projectId: "myproject-7c265",
+    storageBucket: "myproject-7c265.appspot.com",
+    messagingSenderId: "976323657471",
+    appId: "1:976323657471:web:3859037a31265a4a57d6ca",
+    measurementId: "G-GXLHDJ6WQM"
+  };
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+let db = firebase.firestore();
 app.set('view engine', 'ejs');
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
+    let data  = await db.collection("ClassA").get();
+    let userArr = [];
+    data.docs.forEach((doc) => {
+        userArr.push(doc.data().name);
+    })
     res.render("default", {
-        users: ["Alice", "Bob", "Fisheep", "Fiona"],
+        users: userArr,
+        //users: ["Alice", "Bob", "Fisheep", "Fiona"],
         title: "This is root page!"
     })
 })
